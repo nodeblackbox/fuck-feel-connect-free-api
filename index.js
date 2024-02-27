cd7100fdf73ffbde82f038acf3409c3f2494f38d
NASA <65771915+nodeblackbox@users.noreply.github.com>
2024-02-27 07:15:56 +0000

    Socket integration
    test code

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-const fs = require('fs');
-
+const http = require('http');
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 require('dotenv').config();
 
-const port = 3000;
-
-
+const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/', async (req, res) => {
-    res.render('index');
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index'); // For the masseuse
 });
 
-app.get('/test', async (req, res) => {
-    res.render('test');
+app.get('/receiver', (req, res) => {
+    res.render('receiver'); // For the receiver
 });
 
-app.post('/api', async (req, res) => {
-    res.render('api');
+// Handle Socket.IO connections
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('sendCommand', (command) => {
+        console.log('Command received: ', command);
+        io.emit('receiveCommand', command); // Emit to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
